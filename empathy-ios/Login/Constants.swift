79d09dc9c8e2d18635cbce5af16c668924c296fd
Suji Kim <rlatnwl9595@naver.com>
2018-11-21 13:26:05 +0900

    add : Constants.swift

---
+++ empathy-ios/Login/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  empathy-ios
+//
+//  Created by Suji Kim on 21/11/2018.
+//  Copyright © 2018 tucan9389. All rights reserved.
+//
+
+import Foundation
+
+struct INSTAGRAM_IDS {
+    static let INSTAGRAM_AUTHURL = "https://api.instagram.com/oauth/authorize/"
+    static let INSTAGRAM_APIURl  = "https://api.instagram.com/v1/users/"
+    static let INSTAGRAM_CLIENT_ID  = "REPLACE_YOUR_CLIENT_ID_HERE"
+    static let INSTAGRAM_CLIENTSERCRET = "REPLACE_YOUR_CLIENT_SECRET_HERE"
+    static let INSTAGRAM_REDIRECT_URI = "REPLACE_YOUR_REDIRECT_URI_HERE"
+    static let INSTAGRAM_ACCESS_TOKEN =  "access_token"
+    static let INSTAGRAM_SCOPE = "basic+public_content"
+}
--- empathy-ios/Login/LoginViewController.swift
+++ empathy-ios/Login/LoginViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import WebKit
 import FacebookLogin
 
 class LoginViewController: UIViewController {
