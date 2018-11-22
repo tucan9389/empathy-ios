aa7251cee78bb6aaecd1f772db98bed99ac7feb5
Suji Kim <rlatnwl9595@naver.com>
2018-11-22 23:59:44 +0900

    add : Constants.swift file

---
+++ empathy-ios/Login/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  empathy-ios
+//
+//  Created by Suji Kim on 21/11/2018.
+//  Copyright © 2018 tucan9389. All rights reserved.
+//
+
+import Foundation
+
+struct INSTAGRAM_IDS {
+    static let INSTAGRAM_AUTHURL = "https://api.instagram.com/oauth/authorize/"
+    static let INSTAGRAM_APIURl  = "https://api.instagram.com/v1/users/"
+    static let INSTAGRAM_CLIENT_ID = "YOUR_CLIENT_ID"
+    static let INSTAGRAM_CLIENTSERCRET = "YOUR_CLIENT_SECRET"
+    static let INSTAGRAM_REDIRECT_URI = "ENTER_REDIRECT_URI"
+    static let INSTAGRAM_ACCESS_TOKEN =  "access_token"
+    static let INSTAGRAM_SCOPE = "basic+public_content"
+}
