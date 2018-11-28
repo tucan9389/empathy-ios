b2399a9d1ed32d89ea0fd3d8e92b00d51531858b
byungtak <>
2018-11-29 03:53:45 +0900

    add FeedDetailViewController with design

---
+++ empathy-ios/FeedDetail/FeedDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FeedDetailViewController.swift
+//  empathy-ios
+//
+//  Created by byungtak on 29/11/2018.
+//  Copyright © 2018 tucan9389. All rights reserved.
+//
+
+import UIKit
+
+class FeedDetailViewController: UIViewController {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        userImage.layer.cornerRadius = userImage.frame.size.width / 2
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
