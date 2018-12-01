8bf775eaa76cc812740f1d7f4501a75c7c42a605
Suji Kim <rlatnwl9595@naver.com>
2018-12-01 23:39:45 +0900

    add : server connection at my journeyList

--- empathy-ios/New Group/New Group/MainFeedViewController.swift
+++ empathy-ios/New Group/New Group/MainFeedViewController.swift
@@ -34,16 +34,14 @@ class MainFeedViewController: UIViewController {
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toMyFeed", let destination = segue.destination as? MyFeedViewController {
+            destination.userInfo = self.userInfo
+        }
     }
-    */
-
 }
 
 extension MainFeedViewController: UICollectionViewDelegate,UICollectionViewDataSource {
